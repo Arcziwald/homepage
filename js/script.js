26738e4e73e5df6c993efff6a8e30661a970bb9a
Artur Mochnia <amochnia@gmail.com>
2023-05-15 14:25:16 +0100

    variable names fix

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
 {
   const welcome = () => {
-  console.log("Witam wszystkich")
+    console.log("Witam wszystkich");
+  };
+
+  const onButtonClick = () => {
+    const body = document.querySelector(".js-body");
+    const changeBackgroundButton = document.querySelector(
+      ".changeBackgroundButton"
+    );
+
+    body.classList.toggle("darkBackground");
+    changeBackgroundButton.innerText = body.classList.contains("darkBackground")
+      ? "jasny"
+      : "ciemny";
+  };
+
+  const init = () => {
+    const buttonElement = document.querySelector(".header__buttonDeleteJob");
+    const headerElement = document.querySelector(".header__headingjob");
+    const button = document.querySelector("button");
+
+    buttonElement.addEventListener("click", onButtonClick);
+    button.addEventListener("click", onButtonClick);
+    headerElement.remove();
+    buttonElement.remove();
+  };
+
+  welcome();
+
+  init();
 }
-
-const onbuttonClick = () => {
-  const body = document.querySelector("body");
-  const changebBackgroundButton = document.querySelector(".changebBackgroundButton")
-
-  body.classList.toggle("darkBackground");
-  changebBackgroundButton.innerText = body.classList.contains("darkBackground")
-    ? "jasny"
-    : "ciemny";
-}
-
-const init = () => {
-  const buttonElement = document.querySelector(".header__buttonDeleteJob");
-  const headerElement = document.querySelector(".header__headingjob");
-  const button = document.querySelector("button");
-
-  buttonElement.addEventListener("click", onbuttonClick);
-  button.addEventListener("click", onbuttonClick);
-  headerElement.remove();
-  buttonElement.remove();
-}
-
-welcome();
-
-init();
-};
